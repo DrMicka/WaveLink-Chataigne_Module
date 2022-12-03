70a0bed2e3294e48c48542d43da92bedd9964ef0
DrMicka <112657441+DrMicka@users.noreply.github.com>
2022-12-03 18:09:33 +0100

    UPdate for wave link 1.6
    For the moment it is just no possible to toggle the filter

--- WaveLink.js
+++ WaveLink.js
@@ -26,7 +26,7 @@ function update()
 {
 	if (ID == 0) {//si Wavelink repond on est wlObjé, on demande l'état des channels
 		ID = 1;
-		local.send('{"jsonrpc":"2.0","method":"getAllChannelInfo","id":'+ID+'}');
+		local.send('{"jsonrpc":"2.0","method":"getApplicationInfo","id":'+ID+'}');
 	}
 	if (local.parameters.connected == false){
 		ID = 0;
@@ -35,11 +35,23 @@ function update()
 
 function wsMessageReceived(data) { //check si WaveLink répond
 	var wlObj = JSON.parse(data);
-	if (wlObj.id == 1) {//si Wavelink repond on est wlObjé, on demande l'état des channels
+	if (wlObj.id == 1) {//si Wavelink repond, on demande l'état du micro
 		ID ++;
-		local.send('{"jsonrpc":"2.0","method":"getAllChannelInfo","id":'+ID+'}');
+		local.send('{"jsonrpc":"2.0","method":"getMicrophoneConfig","id":'+ID+'}');
 	}
-	else if (wlObj.id == 2) {//si Wavelink repond, on prend les données  
+	else if (wlObj.id == 2) {//si Wavelink repond, on demande l'état des channels
+		ID ++;
+		local.send('{"jsonrpc":"2.0","method":"getOutputs","id":'+ID+'}');
+	}
+	else if (wlObj.id == 3) {//si Wavelink repond, on demande l'état des channels
+		ID ++;
+		local.send('{"jsonrpc":"2.0","method":"getOutputConfig","id":'+ID+'}');
+	}
+	else if (wlObj.id == 4) {//si Wavelink repond, on demande l'état des channels
+		ID ++;
+		local.send('{"jsonrpc":"2.0","method":"getInputConfigs","id":'+ID+'}');
+	}	
+	else if (wlObj.id == 5) {//si Wavelink repond, on prend les données  
 		statut = JSON.parse(data);
 	//on supprime les anciennes valeurs
 		removeValues();
@@ -48,17 +60,17 @@ function wsMessageReceived(data) { //check si WaveLink répond
 		while (statut['result'][n].bgColor != null) {	
 		//on enregistre les nouvelles données des channels (mixer name et mix id) dans les values
 			local.values.addContainer("AudioChannel"+n);
-			local.values.getChild("AudioChannel"+n).addStringParameter("Mixer name","", statut['result'][n].mixerName);
+			local.values.getChild("AudioChannel"+n).addStringParameter("Mixer name","", statut['result'][n].name);
 			local.values.getChild("AudioChannel"+n).getChild("mixerName").setAttribute("readonly" ,true);
-			local.values.getChild("AudioChannel"+n).addStringParameter("Mix ID","", statut['result'][n].mixId);
-			local.values.getChild("AudioChannel"+n).getChild("mixId").setAttribute("readonly" ,true);
-			local.values.getChild("AudioChannel"+n).addBoolParameter("Stream muted","To know if the stream audio channel's is muted or not",statut['result'][n].isStreamInMuted);
+			local.values.getChild("AudioChannel"+n).addStringParameter("Identifier","", statut['result'][n].identifier);
+			local.values.getChild("AudioChannel"+n).getChild("identifier").setAttribute("readonly" ,true);
+			local.values.getChild("AudioChannel"+n).addBoolParameter("Stream muted","To know if the stream audio channel's is muted or not",statut['result'][n]['streamMixer'][0]);
 			local.values.getChild("AudioChannel"+n).getChild("streamMuted").setAttribute("readonly" ,true);
-			local.values.getChild("AudioChannel"+n).addFloatParameter("Stream volume","The level of stream volume of this channel",statut['result'][n].streamVolumeIn,0,100);
+			local.values.getChild("AudioChannel"+n).addFloatParameter("Stream volume","The level of stream volume of this channel",statut['result'][n]['streamMixer'][1],0,100);
 			local.values.getChild("AudioChannel"+n).getChild("streamVolume").setAttribute("readonly" ,true);
-			local.values.getChild("AudioChannel"+n).addBoolParameter("Local muted","To know if the local audio channel's is muted or not",statut['result'][n].isLocalInMuted);
+			local.values.getChild("AudioChannel"+n).addBoolParameter("Local muted","To know if the local audio channel's is muted or not",statut['result'][n]['localMixer'][0]);
 			local.values.getChild("AudioChannel"+n).getChild("localMuted").setAttribute("readonly" ,true);
-			local.values.getChild("AudioChannel"+n).addFloatParameter("Local volume","The level of local volume of this channel",statut['result'][n].localVolumeIn,0,100);
+			local.values.getChild("AudioChannel"+n).addFloatParameter("Local volume","The level of local volume of this channel",statut['result'][n]['localMixer'][1],0,100);
 			local.values.getChild("AudioChannel"+n).getChild("localVolume").setAttribute("readonly" ,true);
 			
 		//on converti les true et false
@@ -77,6 +89,18 @@ function wsMessageReceived(data) { //check si WaveLink répond
 			if (statut['result'][n].streamMixFilterBypass == true){
 				statut['result'][n].streamMixFilterBypass = "true";
 			} else {statut['result'][n].streamMixFilterBypass = "false";}
+			if (statut['result'][n]['streamMixer'][0] == true){
+				statut['result'][n]['streamMixer'][0] = "true";
+			} else {statut['result'][n]['streamMixer'][0] = "false";}
+			if (statut['result'][n]['streamMixer'][2] == true){
+				statut['result'][n]['streamMixer'][2] = "true";
+			} else {statut['result'][n]['streamMixer'][2] = "false";}
+			if (statut['result'][n]['localMixer'][0] == true){
+				statut['result'][n]['localMixer'][0] = "true";
+			} else {statut['result'][n]['localMixer'][0] = "false";}
+			if (statut['result'][n]['localMixer'][2] == true){
+				statut['result'][n]['localMixer'][2] = "true";
+			} else {statut['result'][n]['localMixer'][2] = "false";}
 			
 		//on enregistre les nouvelles données des filtres des channels 
 			local.values.getChild("AudioChannel"+n).addFloatParameter("Filters number","",0);
@@ -87,12 +111,12 @@ function wsMessageReceived(data) { //check si WaveLink répond
 				local.values.getChild("AudioChannel"+n).addContainer("Filters");
 				local.values.getChild("AudioChannel"+n).getChild("Filters").addStringParameter("Filter name "+i,"", statut['result'][n]['filters'][i].name);
 				local.values.getChild("AudioChannel"+n).getChild("Filters").getChild("filterName"+i).setAttribute("readonly" ,true);
-				local.values.getChild("AudioChannel"+n).getChild("Filters").addBoolParameter("Filter name "+i+" active","To know if this filter is activate or not",statut['result'][n]['filters'][i].active);
+				local.values.getChild("AudioChannel"+n).getChild("Filters").addBoolParameter("Filter name "+i+" active","To know if this filter is activate or not",statut['result'][n]['filters'][i].isActive);
 				local.values.getChild("AudioChannel"+n).getChild("Filters").getChild("filterName"+i+"active").setAttribute("readonly" ,true);
 			//On change les true et false en texte
-				if (statut['result'][n]['filters'][i].active == true){
-					statut['result'][n]['filters'][i].active = "true";
-				} else {statut['result'][n]['filters'][i].active = "false";}
+				if (statut['result'][n]['filters'][i].isActive == true){
+					statut['result'][n]['filters'][i].isActive = "true";
+				} else {statut['result'][n]['filters'][i].isActive = "false";}
 			//On enregistre la nouvelle valeur du nombre de filtres
 				i++;
 				local.values.getChild("AudioChannel"+n).getChild("filtersNumber").set(i);
@@ -106,11 +130,7 @@ function wsMessageReceived(data) { //check si WaveLink répond
 	ID ++;
 	local.send('{"jsonrpc":"2.0","method":"getSwitchState","id":'+ID+'}');
 	}
-	else if (wlObj.id == 3) {//si Wavelink repond, on prend les données  et on demande l'état des moniteurs
-	ID ++;
-	local.send('{"jsonrpc":"2.0","method":"getMonitoringState","id":'+ID+'}');
-	}
-	else if (wlObj.id == 4) {//si Wavelink repond, on prend les données 
+	else if (wlObj.id == 6) {//si Wavelink repond, on prend les données  et on demande l'état des moniteurs
 
 	}
 };
@@ -126,7 +146,8 @@ function removeValues () {
 		}
 		local.values.getChild("AudioChannelNumber").set(0);
 };
-	
+
+//à modifier en fonction des besoins lors du toggle filter	
 function FiltersList(index) {
 	var n = 0;
 	var filters ="";
@@ -143,11 +164,11 @@ function FiltersList(index) {
 function setupEnumChannel(command) {
     var enumChan = command.addEnumParameter("Toggle channel", "");
 	var enumTypVol = command.addEnumParameter("Type of volume", "");
-	enumTypVol.addOption("Local mix","local");
-	enumTypVol.addOption("Stream mix","stream");
+	enumTypVol.addOption("Local mix","com.elgato.mix.local");
+	enumTypVol.addOption("Stream mix","com.elgato.mix.stream");
 	var index = 0;
 	while (statut['result'][index].bgColor != null) {
-		enumChan.addOption(statut['result'][index].mixerName,statut['result'][index].mixId);
+		enumChan.addOption(statut['result'][index].name,statut['result'][index].identifier);
 		index++;
 	}
  }
@@ -159,36 +180,35 @@ function setupToggleFilter(command) {
 	var n = 0;
 	while (statut['result'][index].bgColor != null) {
 		while (statut['result'][index]['filters'][n].filterID != null) {
-			var choice = statut['result'][index].mixerName+' - '+statut['result'][index]['filters'][n].name;
+			var choice = statut['result'][index].name+' - '+statut['result'][index]['filters'][n].name;
 			enumFilt.addOption(choice,n);
 			n++;
 		}
-		enumChan.addOption(statut['result'][index].mixerName,statut['result'][index].mixId);
+		enumChan.addOption(statut['result'][index].name,statut['result'][index].identifier);
 		index++;
 	}
  }
 
-function Commande() {
-	var path = local.scripts.waveLink.params.getChild("fileParam").getAbsolutePath();
-	script.log("path :"+path);
+function Commande(data) {
+	local.send(data);
 }
 /* 	--------------------------------------------------------------------------------------------------------------
 											Set Volume
 	--------------------------------------------------------------------------------------------------------------*/
 
-function setVolume(Vol, idMix, Type) {
-//1. trouver l'index du channel (mixID) vérifier si le son choisi est mute ou unmute et changer sa valeur
+function setVolume(Vol, identifier, Type) {
+//1. trouver l'index du channel (identifier)
 	var index;
 	var n = 0;
 	while (statut['result'][n].bgColor != null) {
-		if (statut['result'][n].mixId == idMix && Type == "stream") {
+		if (statut['result'][n].identifier == identifier && Type == "com.elgato.mix.stream") {
 			index = n;
-			statut['result'][n].streamVolumeIn = Vol;
+			statut['result'][n]['streamMixer'][1] = Vol;
 			local.values.getChild("AudioChannel"+n).getChild("streamVolume").set(Vol);
 		}
-		else if (statut['result'][n].mixId == idMix && Type == "local") { 
+		else if (statut['result'][n].identifier == identifier && Type == "com.elgato.mix.local") { 
 			index = n;
-			statut['result'][n].localVolumeIn = Vol;
+			statut['result'][n]['localMixer'][1] = Vol;
 			local.values.getChild("AudioChannel"+n).getChild("localVolume").set(Vol);
 		}
 	n++;
@@ -197,37 +217,40 @@ function setVolume(Vol, idMix, Type) {
 //2. on sauvegarde les nouveaux paramètres et on envoi la commande
 	local.values.saveInfoWL.trigger();
 	ID ++;
-	local.send('{"jsonrpc": "'+statut.jsonrpc+'","method":"setInputMixer","id":'+ID+',"params":{"mixId":"'+idMix+'","slider":"'+Type+'","localVolumeIn":'+statut['result'][index].localVolumeIn+',"isLocalInMuted":'+statut['result'][index].isLocalInMuted+',"streamVolumeIn":'+statut['result'][index].streamVolumeIn+',"isStreamInMuted":'+statut['result'][index].isStreamInMuted+',"filters":['+FiltersList(index)+'],"localMixFilterBypass":'+statut['result'][index].localMixFilterBypass+',"streamMixFilterBypass":'+statut['result'][index].streamMixFilterBypass+'}}');
-	
+	local.send('{"jsonrpc": "2.0","method": "setInputConfig","id": '+ID+',"params": {"property": "Volume","identifier": '+JSON.stringify(statut['result'][index].identifier)+',"mixerID": '+JSON.stringify(Type)+',"value": '+Vol+',"forceLink": false}}');
 }
 
 /* 	--------------------------------------------------------------------------------------------------------------
 											toggle mute source
 	--------------------------------------------------------------------------------------------------------------*/
 
-function toggleMuteVolume(idMix, Type) {
-//1. trouver l'index du channel (mixID) vérifier si le son choisi est mute ou unmute et changer sa valeur
+function toggleMuteVolume(identifier, Type) {
+//1. trouver l'index du channel (identifier) vérifier si le son choisi est mute ou unmute et changer sa valeur
 	var index;
 	var n = 0;
 	while (statut['result'][n].bgColor != null) {
-		if (statut['result'][n].mixId == idMix && Type == "stream" && statut['result'][n].isStreamInMuted == "true") {
+		if (statut['result'][n].identifier == identifier && Type == "com.elgato.mix.stream" && statut['result'][n]['streamMixer'][0] == "true") {
 			index = n;
-			statut['result'][n].isStreamInMuted = "false";
+			statut['result'][n]['streamMixer'][0] = "false";
+			var value = "false";
 			local.values.getChild("AudioChannel"+n).getChild("streamMuted").set(false);
 		}
-		else if (statut['result'][n].mixId == idMix && Type == "stream" && statut['result'][n].isStreamInMuted == "false") { 
+		else if (statut['result'][n].identifier == identifier && Type == "com.elgato.mix.stream" && statut['result'][n]['streamMixer'][0] == "false") { 
 			index = n;
-			statut['result'][n].isStreamInMuted = "true";
+			statut['result'][n]['streamMixer'][0] = "true";
+			var value = "true";
 			local.values.getChild("AudioChannel"+n).getChild("streamMuted").set(true);
 	}
-		else if (statut['result'][n].mixId == idMix && Type == "local" && statut['result'][n].isLocalInMuted == "true") {
+		else if (statut['result'][n].identifier == identifier && Type == "com.elgato.mix.local" && statut['result'][n]['localMixer'][0] == "true") {
 			index = n;
-			statut['result'][n].isLocalInMuted = "false";
+			statut['result'][n]['localMixer'][0] = "false";
+			var value = "false";
 			local.values.getChild("AudioChannel"+n).getChild("localMuted").set(false);
 		}
-		else if (statut['result'][n].mixId == idMix && Type == "local" && statut['result'][n].isLocalInMuted == "false") { 
+		else if (statut['result'][n].identifier == identifier && Type == "com.elgato.mix.local" && statut['result'][n]['localMixer'][0] == "false") { 
 			index = n;
-			statut['result'][n].isLocalInMuted = "true";
+			statut['result'][n]['localMixer'][0] = "true";
+			var value = "true";
 			local.values.getChild("AudioChannel"+n).getChild("localMuted").set(true);
 	}
 	n++;
@@ -236,26 +259,25 @@ function toggleMuteVolume(idMix, Type) {
 //2. On sauvegarde les nouveaux paramètres et on envoi la commande
 	local.values.saveInfoWL.trigger();
 	ID ++;
-	local.send('{"jsonrpc": "'+statut.jsonrpc+'","method":"setInputMixer","id":'+ID+',"params":{"mixId":"'+idMix+'","slider":"'+Type+'","localVolumeIn":'+statut['result'][index].localVolumeIn+',"isLocalInMuted":'+statut['result'][index].isLocalInMuted+',"streamVolumeIn":'+statut['result'][index].streamVolumeIn+',"isStreamInMuted":'+statut['result'][index].isStreamInMuted+',"filters":['+FiltersList(index)+'],"localMixFilterBypass":'+statut['result'][index].localMixFilterBypass+',"streamMixFilterBypass":'+statut['result'][index].streamMixFilterBypass+'}}');
-	
+	local.send('{"jsonrpc": "2.0","method": "setInputConfig","id": '+ID+',"params": {"property": "Mute","identifier": '+JSON.stringify(statut['result'][index].identifier)+',"mixerID": '+JSON.stringify(Type)+',"value": '+value+',"forceLink": false}}');
 }
 
 /* 	--------------------------------------------------------------------------------------------------------------
 										Fonction toggle filter
 	--------------------------------------------------------------------------------------------------------------*/
-function toggleFilter(idMix, numFiltre) {
-//1. trouver l'index du channel (mixID) vérifier si le filtre choisi est mute ou unmute et changer sa valeur
+function toggleFilter(identifier, numFiltre) {
+//1. trouver l'index du channel (identifier) vérifier si le filtre choisi est mute ou unmute et changer sa valeur
 	var index;
 	var n = 0;
 	while (statut['result'][n].bgColor != null) {
-		if (statut['result'][n].mixId == idMix && statut['result'][n]['filters'][numFiltre].active == "true") {
+		if (statut['result'][n].identifier == identifier && statut['result'][n]['filters'][numFiltre].isActive == "true") {
 			index = n;
-			statut['result'][n]['filters'][numFiltre].active = "false";
+			statut['result'][n]['filters'][numFiltre].isActive = "false";
 			local.values.getChild("AudioChannel"+n).getChild("Filters").getChild("filterName"+numFiltre+"Active").set(false);
 		}
-		else if (statut['result'][n].mixId == idMix && statut['result'][n]['filters'][numFiltre].active == "false") { 
+		else if (statut['result'][n].identifier == identifier && statut['result'][n]['filters'][numFiltre].isActive == "false") { 
 			index = n;
-			statut['result'][n]['filters'][numFiltre].active = "true";
+			statut['result'][n]['filters'][numFiltre].isActive = "true";
 			local.values.getChild("AudioChannel"+n).getChild("Filters").getChild("filterName"+numFiltre+"Active").set(true);
 		}
 		n++;
@@ -264,5 +286,5 @@ function toggleFilter(idMix, numFiltre) {
 //2. On sauvegarde les nouveaux paramètres et on envoi la commande
 	local.values.saveInfoWL.trigger();
 	ID ++;
-	local.send('{"jsonrpc": "'+statut.jsonrpc+'","method":"setInputMixer","id":'+ID+',"params":{"mixId":"'+idMix+'","slider":"","localVolumeIn":'+statut['result'][index].localVolumeIn+',"isLocalInMuted":'+statut['result'][index].isLocalInMuted+',"streamVolumeIn":'+statut['result'][index].streamVolumeIn+',"isStreamInMuted":'+statut['result'][index].isStreamInMuted+',"filters":['+FiltersList(index)+'],"localMixFilterBypass":'+statut['result'][index].localMixFilterBypass+',"streamMixFilterBypass":'+statut['result'][index].streamMixFilterBypass+'}}');
+	local.send('{"jsonrpc": "2.0","method": "filterChanged","id": '+ID+',"params": {"filterID":"'+statut['result'][index]['filters'][numFiltre].filterID+'","identifier":'+JSON.stringify(statut['result'][index].identifier)+',"value":false}}');
 };
